ab5fbffce8affdc44449c2bbe39b0d397b01dd2e
John Agbanusi <agbanusijohn@gmail.com>
2022-05-03 01:51:09 +0100

    update examples

--- ts/examples/swap.ts
+++ ts/examples/swap.ts
@@ -1,7 +1,6 @@
 import { Connection, Keypair, PublicKey, Transaction } from "@solana/web3.js";
 import { Swap } from "../src";
 
-
 const connection = new Connection(
   "https://api.mainnet-beta.solana.com/",
   "finalized"
@@ -11,11 +10,12 @@ async function main() {
   const wallet = new Keypair();
   const swap = new Swap(connection);
 
+  // wrap SOL token if Sol is first token pair
   const ixs = await swap.createSwapIx(
     new PublicKey("So11111111111111111111111111111111111111112"),
     new PublicKey("EPjFWdd5AufqSSqeM2qN1xzybapC8G4wEGGkZwyTDt1v"),
     100000n, // 0.0001 SOL
-    100n, // 0.0001 USDC 
+    100n, // 0.0001 USDC
     wallet.publicKey
   );
 
@@ -23,6 +23,8 @@ async function main() {
   for (const ix of ixs) {
     tx.add(ix);
   }
+
+  //add unwrap sol token txn if sol is present as resulting token pair
 
   // Send out the tx use browser wallet or keypair
 }
