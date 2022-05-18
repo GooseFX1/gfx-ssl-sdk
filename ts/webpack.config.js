9336a6960c00b0e39678d86ff89e4821cb2cb685
Weiyuan Wu <youngw@sfu.ca>
2022-05-18 07:01:30 +0000

    optimize the artifact

--- ts/webpack.config.js
+++ ts/webpack.config.js
@@ -18,18 +18,11 @@ module.exports = {
                 test: /\.ts$/,
                 use: 'ts-loader',
                 exclude: /node_modules/,
-            },
-            {
-                test: /\.wasm$/,
-                type: "asset/inline",
-            },
+            }
         ]
     },
     resolve: {
-        extensions: ['.ts', '.js', "json", "wasm"],
+        extensions: ['.ts', '.js', "json"],
         fallback: { assert: false, process: false, fs: false, util: false, path: false }
-    },
-    optimization: {
-        minimize: false
     }
 };
