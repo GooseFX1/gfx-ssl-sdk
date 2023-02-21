0528805d0326ed2f0fb67cd2d919af2678eba5c1
eric <ebrightfield@gmail.com>
2023-02-21 14:18:35 -0700

    update to jupiter code example

--- sdk/examples/jupiter.rs
+++ sdk/examples/jupiter.rs
@@ -1,35 +1,62 @@
 use gfx_ssl_sdk::jupiter::GfxAmm;
-use jupiter_core::amm::{Amm, QuoteParams};
+use itertools::Itertools;
+use jupiter_core::amm::{Amm, KeyedAccount, QuoteParams};
 use solana_client::rpc_client::RpcClient;
 use solana_program::pubkey::Pubkey;
 use solana_sdk::pubkey;
 use std::collections::HashMap;
+use std::env::var;
 
-const ETH_MINT: Pubkey = pubkey!("7vfCXTUXx5WJV5JADk17DUJ4ksgau7utNKj4b963voxs");
-const USDC_MINT: Pubkey = pubkey!("EPjFWdd5AufqSSqeM2qN1xzybapC8G4wEGGkZwyTDt1v");
+pub const SOL_USDC: Pubkey = pubkey!("CpfpL9PXt88u3kPQ6fuD6WqQpQ8c5UEftxsop9rm1ATM");
 
 fn main() {
-    let client = RpcClient::new("https://api.mainnet-beta.solana.com");
+    // Acquire the account data for the desired swap pair account.
+    let url =
+        var("SOLANA_RPC_URL").unwrap_or_else(|_| "https://api.mainnet-beta.solana.com".to_string());
+    let client = RpcClient::new(url);
+    let account = client.get_account(&SOL_USDC).unwrap();
 
-    let mut amm = GfxAmm::new(ETH_MINT, USDC_MINT).unwrap();
+    // Use the [GfxAmm::from_keyed_account] constructor.
+    let keyed_account = KeyedAccount {
+        key: SOL_USDC,
+        account,
+        params: None
+    };
+    let mut amm = GfxAmm::from_keyed_account(keyed_account).unwrap();
+    println!("Pair: {:?}", amm.pair_pubkey);
 
-    // Initialize the account state
-    let acts_to_update = amm.get_accounts_to_update();
-    let acts_data = client
-        .get_multiple_accounts(&acts_to_update)
-        .unwrap()
-        .into_iter()
-        .map(|act| act.unwrap().data)
-        .collect::<Vec<_>>();
-    let acts: HashMap<Pubkey, Vec<u8>> = acts_to_update.into_iter().zip(acts_data).collect();
-    amm.update(&acts).unwrap();
+    // Update the account state to fetch fresh oracle data
+    let accounts_to_update = amm.get_accounts_to_update();
+    let mut acts_data = HashMap::new();
+    for chunk in &accounts_to_update.iter().chunks(5) {
+        let accs: Vec<_> = Vec::from_iter(chunk.copied());
+        acts_data.extend(
+            client
+                .get_multiple_accounts(&accs)
+                .unwrap()
+                .into_iter()
+                .zip(accs)
+                .map(|(act, key)| (key, act.unwrap().data)),
+        );
+    }
+    amm.update(&acts_data).unwrap();
 
-    let quote = amm
-        .quote(&QuoteParams {
-            in_amount: 40000,
-            input_mint: ETH_MINT,
-            output_mint: USDC_MINT,
-        })
-        .unwrap();
-    println!("{:?}", quote);
+    // Get a quote
+    let mints = amm.get_reserve_mints();
+    match amm.quote(&QuoteParams {
+        in_amount: (1. / 20. * 1e9) as u64,
+        input_mint: mints[0],
+        output_mint: mints[1],
+    }) {
+        Ok(quote) => {
+            println!("Quote: {:#?}", quote);
+            let mut price = (quote.in_amount as f64 / 1e9)
+                / (quote.out_amount as f64 / 1e6);
+            price = 1. / price;
+            println!("Price: {}\n", price);
+        }
+        Err(e) => {
+            println!("Error: {e}\n");
+        }
+    }
 }
